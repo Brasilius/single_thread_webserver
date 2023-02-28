86ae010af56b65db0041d9bcb4de2ab5ce4ca89e
niels <Niels.L.Larsen-1@ou.edu>
2023-02-27 21:23:49 -0600

    work done for the day

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::net::TcpListener;
+use std::io::{prelude::*, BufReader};
+use std::net::TcpStream;
+
 fn main() {
     println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7777").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("conncection established!");
+        handle_connection(stream);
+    }
+}
+fn handle_connection(mut stream: TcpStream){
+    let buf_reader = BufReader::new(&mut stream);
+    let https_request: Vec<_> = buf_reader.lines().map(|result| result.unwrap()).take_while(|line| !line.is_empty()).collect();
+    println!("{:?}", https_request);
+
+
+
+
 }
